9f19111cc07af9e53dbab29853642a48df377bcd
Ali Gel <ali@gel.com>
2024-08-14 23:30:31 +0300

    initial

---
+++ src/test/java/utilities/Authentication.java
@@ -0,0 +1,29 @@
+package utilities;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
+
+
+public class Authentication {
+
+    public static String generateToken(){
+        Map<String,String> credential = new HashMap<>();
+
+        credential.put("email",ConfigReader.getProperty("contact_list_username"));
+        credential.put("password",ConfigReader.getProperty("contact_list_password"));
+
+        Response response =given().
+                                  body(credential).
+                                  contentType(ContentType.JSON).
+                                  post("https://thinking-tester-contact-list.herokuapp.com/users/login");
+        return response.jsonPath().getString("token");
+    }
+
+
+}
